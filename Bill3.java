94f23d93e5d8fe7a781dbfcb2e7004030348d5e6
yahelil <yahelil.levity@gmail.com>
2024-11-05 00:01:07 +0200

    Checking the basic result

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,17 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+        String name2 = args[1];
+        String name3 = args[2];
+
+		double totalBill = Double.parseDouble(args[3]);
+		float totalBillfloat = (float) Math.ceil(totalBill);
+		float share = totalBillfloat/3;
+
+		System.out.println(name1 + "'s share: $" + share);
+        System.out.println(name2 + "'s share: $" + share);
+        System.out.println(name3 + "'s share: $" + share);
+
+		System.out.println("Total bill: $" + totalBill);
 	}
 }
